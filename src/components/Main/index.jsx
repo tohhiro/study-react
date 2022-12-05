21f33e09d5d12307df5c471782b19c14cba8bb64
hiroto <tohhiro@gmail.com>
2022-12-05 11:51:21 +0900

    ✨ 講座用のuseEffectを導入

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
 import classes from "src/components/Main/Main.module.css";
 
 export function Main(props) {
   const { page } = props;
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue"
+    return () => {
+      document.body.style.backgroundColor = ""
+    };
+  },[]);
+
   return (
     <main className={classes.main}>
       <Headline page={page}>
