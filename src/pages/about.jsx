bfb349c3a0c70cd325fb68c30e751bc277ae5faf
hiroto <tohhiro@gmail.com>
2022-12-24 16:59:01 +0900

    ✨ ページ間のStateのリフトアップ

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,15 +3,11 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 import classes from "src/styles/Home.module.css";
-import { useCounter } from "src/hooks/useCounter"
-import { useInputArray } from "src/hooks/useInputArray"
-import { useBgLightBlue } from "src/hooks/useBgLightBlue"
 
-export default function About() {
-  const {count, isShow, handleClick, handleDisplay} = useCounter()
-  const  {text, array, handleChange, handleAdd} = useInputArray()
-  useBgLightBlue()
 
+export default function About(props) {
+  const  {count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd } = props
+  
   return (
     <div className={classes.container}>
       <Head>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,10 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 import classes from "src/styles/Home.module.css";
-import { useCounter } from "src/hooks/useCounter"
-import { useInputArray } from "src/hooks/useInputArray"
-import { useBgLightBlue } from "src/hooks/useBgLightBlue"
 
 
-export default function Home() {
-  const {count, isShow, handleClick, handleDisplay} = useCounter()
-  const  {text, array, handleChange, handleAdd} = useInputArray()
-  useBgLightBlue()
+export default function Home(props) {
+  const  {count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd } = props
 
   return (
     <div className={classes.container}>
